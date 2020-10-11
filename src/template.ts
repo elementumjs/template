6592edf2782c4a47fd863aafd865de8f3e715f61
Lucas Menendez <epucas@gmail.com>
2020-10-11 12:36:18 +0200

    update dependencies versions and export whole class definition

--- src/template.ts
+++ src/template.ts
@@ -1,6 +1,6 @@
 import { Template } from './lib/template';
 import { Processor } from './lib/processor';
 
-export const TemplateClass = Template;
+export { Template };
 export const html = (strings: TemplateStringsArray, ...args: Array<any>) => new Template(strings.raw.slice(0), args);
 export const render = (template: Template, container: HTMLElement) => new Processor(template, container).render();
