ef55a53271d2b6e08b8b81e304eb672a27fa959c
Lucas Menéndez <epucas@gmail.com>
2021-08-25 20:47:03 +0200

    processor.ts commented

--- src/lib/processor.ts
+++ src/lib/processor.ts
@@ -40,17 +40,20 @@ class Processor {
     }
 
     /**
-     * getSlot method iterates over the current template definition slots
-     * searching for a slot with the same index that the provided one.
-     * @param {number} slotIndex The index of the slot to search.
+     * getSlot method iterates over the current template definition 
+     * {@link Slot}'s searching for a {@link Slot} with the same index that the 
+     * provided one.
+     * @param {number} slotIndex The index of the {@link Slot} to search.
      * @returns {Slot} - The desired slot.
      */
     private getSlot(index: number): Slot {
-        // Iterates over the template slots to get the correct one by provided
-        // index.
+        /**
+         * Iterates over the template {@link Slot}'s to get the correct one by 
+         * provided index.
+         */
         const { length } = this.template.slots;
         for (let i: number = 0; i < length; i++) {
-            // Search for the slot with the current index.
+            /** Search for the {@link Slot} with the current index. */
             const slot: Slot = this.template.slots[i];
             if (slot.slotIndex === index) return slot;
         }
@@ -62,40 +65,51 @@ class Processor {
 
     /**
      * render checks if the template is already rendered into the container 
-     * after injecting it. If it was rendered, it iterates over template slots 
-     * to render them. If it was not rendered, it appends the template to the 
-     * container.
+     * after injecting it. If it was rendered, it iterates over template 
+     * {@link Slot}'s to commit them. If it was not rendered, it appends 
+     * the {@link Template} to the container.
      */
     public render(): void {
-        // Creates a {@link TreeWalker} to iterates over container child nodes.
-        // It uses acceptFilter to get the slot marks ({@link Comment} elements 
-        // that includes an index).
+        /**
+         * Creates a {@link TreeWalker} to iterates over container child nodes.
+         * It uses {@link acceptNode} to get the {@link Slot} marks 
+         * ({@link Comment} elements that includes an index).
+         */
         const walker: NodeIterator = document.createNodeIterator(
             this.container,
             NodeFilter.SHOW_COMMENT,
             { acceptNode }
         );
         
-        // Gets the first mark founded to check if the container has the 
-        // template already rendered.
+        /**
+         * Gets the first mark founded to check if the container has the 
+         * {@link Template} already rendered.
+         */
         let current: Node = walker.nextNode();
         if (current === null) {
-            // If the first mark founded is null, the template was not detected 
-            // and it is appended to the container.
+            /**
+             * If the first mark was founded is null, the {@link Template} was 
+             * not detected and append it to the container.
+             */
             this.container.appendChild(this.template.element);
             return;
         } 
 
+        /**
+         * Store the last founded {@link Slot} slot index because the 
+         * {@link Slot}'s of a {@link Template} have consecutive indexes, so the
+         * next valid {@link Slot} must have a higher {@link Slot} index than 
+         * the previous one. If not, the next {@link Slot} is relative to 
+         * another child {@link Template}.
+         */
         let lastSlotIndex: number = 0;
         while (current) {
-            // Iterate over the found marks getting their slot index and 
-            // committing the current node (the sibling of the {@link Node}
-            // mark).
+            /**
+             * Iterate over the found marks getting their {@link Slot} index and 
+             * committing the current {@link Node} (the sibling of the 
+             * {@link Node} mark).
+             */
             const slotIndex: number = parseInt(current.nodeValue);
-            
-            // The template slots have consecutive slot indexes, so the next 
-            // valid slot must have a higher slot index than the previous one. 
-            // If not, the next slot is relative to another child template.
             if (slotIndex > lastSlotIndex) {
                 const slot: Slot = this.getSlot(slotIndex);
                 slot.commit(current);
