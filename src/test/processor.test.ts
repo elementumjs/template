c73aa55582349d1171f06ef17ea96c992298b930
Lucas Menendez <epucas@gmail.com>
2021-10-28 00:09:25 +0200

    some tests fixed

--- src/test/processor.test.ts
+++ src/test/processor.test.ts
@@ -4,10 +4,14 @@ import { Processor } from '../lib/processor';
 test("Processor.render", () => {
     const emptyContainer = document.createElement('container');
     const processor = new Processor(html`<p>${'Test'}</p>`, emptyContainer);
+    let commitHandler = jest.spyOn(processor.template.slots[0] as any, 'commit');
     processor.render();
     expect(emptyContainer.innerHTML).toBe('<p><!--1-->Test<!-----></p>');
+    expect(commitHandler).toHaveBeenCalledTimes(0);
 
     processor.template = html`<p>${'Ey!'}</p>`;
+    commitHandler = jest.spyOn(processor.template.slots[0] as any, 'commit');
     processor.render();
     expect(emptyContainer.innerHTML).toBe('<p><!--1-->Ey!<!-----></p>');
+    expect(commitHandler).toHaveBeenCalled();
 });
