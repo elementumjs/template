7413e6adf90c853c5ab8736ddd1c5aef96bd75f9
Lucas Menendez <epucas@gmail.com>
2021-10-29 16:11:56 +0200

    final tests

--- src/test/processor.test.ts
+++ src/test/processor.test.ts
@@ -4,14 +4,17 @@ import { Processor } from '../lib/processor';
 test("Processor.render", () => {
     const emptyContainer = document.createElement('container');
     const processor = new Processor(html`<p>${'Test'}</p>`, emptyContainer);
-    let commitHandler = jest.spyOn(processor.template.slots[0] as any, 'commit');
+    
+    const parentHandler = jest.spyOn(processor.container as any, 'appendChild');
     processor.render();
     expect(emptyContainer.innerHTML).toBe('<p><!--1-->Test<!-----></p>');
-    expect(commitHandler).toHaveBeenCalledTimes(0);
-
+    expect(parentHandler).toHaveBeenCalledTimes(1);
+    
     processor.template = html`<p>${'Ey!'}</p>`;
-    commitHandler = jest.spyOn(processor.template.slots[0] as any, 'commit');
+    const slotsHandler = jest.spyOn(processor as any, 'getSlots');
+    const commitHandler = jest.spyOn(processor.template.slots[0] as any, 'commit');
     processor.render();
     expect(emptyContainer.innerHTML).toBe('<p><!--1-->Ey!<!-----></p>');
     expect(commitHandler).toHaveBeenCalled();
+    expect(slotsHandler).toHaveBeenCalled();
 });
